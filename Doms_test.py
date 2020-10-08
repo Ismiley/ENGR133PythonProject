04b12c1d5b72a764c6649ad4fb5cd7edabca6226
Dominick Caponigro <dominickbeast@gmail.com>
2020-10-08 14:08:07 -0400

    Despite its name, this is the final grayscale function code

--- Doms_test.py
+++ Doms_test.py
@@ -22,8 +22,66 @@ Contributors:   Ismail Husain, husain6@purdue
 	have to list that person as a contributor here as well.
 ===============================================================================
 '''
+import matplotlib.image as mpl
+import numpy as num
+import sys as s
 
-print('Hi this is a test!')
+class Error1(FileNotFoundError):
+    pass
+
+try:
+    Input=input('File Name: ')
+except FileNotFoundError:
+    raise Error1('File name incorrect, dont forget the file type')
+
+# Takes a file path and returns the array of the image
+def importImage(x): #x is the file path as a string
+    global image # to show image in variable explorer for analysis
+    image = mpl.imread(x)
+    image=image #here for debug sake
+    return image
+
+def RGBtoGRAY(rgb):
+    global gray
+    for i in range(len(rgb)):
+        for j in range(len(rgb[0])):
+            r=rgb[i][j][0]
+            g=rgb[i][j][1]
+            b=rgb[i][j][2]           
+            gray=(0.2989*r+0.5870*g+0.1140*b)
+            rgb[i][j][0],rgb[i][j][1],rgb[i][j][2]=gray,gray,gray
+    return rgb
+
+GOutput=open('Grayscale.png','wb')
+importImage(Input)
+RGBtoGRAY(image)
+
+mpl.imsave('Grayscale.png',RGBtoGRAY(image))
+
+
+
+
+#g=0
+#for i in Input:
+#    importImage(Input)
+#    RGBtoGRAY(image)
+#    GOutput.write(gray)
+#    g=g+1.1
+#    if g%11==0:
+#        print(g)
+#    else:
+#        pass
+    
+    
+    
+    
+#    print(importImage)
+#    s.exit()
+
+
+
+GOutput.close
+
 
 
 '''
