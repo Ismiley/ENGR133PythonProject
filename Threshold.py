3618bfc45587e7349104e568849efe69453849ee
Ismail Husain <husaisma@gmail.com>
2020-10-08 15:10:48 -0400

    Added comments

--- Threshold.py
+++ Threshold.py
@@ -9,18 +9,21 @@ Created on Tue Oct  6 15:03:52 2020
 
 import matplotlib.image as mpimg
 
-
+#get image location
 path = input('Input image file name (in folder): ')
 
+##Function to import image
 def importImage(path):
     image = mpimg.imread(path)
     return image
 
-
+#import image
 image = importImage(path)
 
 
-
+##Threshold function
+#Takes each RGB value, converts to 8 bit number,
+#converts to black (0) or white (255) if threshold value 160 is met
 def Threshold(image):
     iRows = len(image)
     iCols = len(image[0])
@@ -35,8 +38,9 @@ def Threshold(image):
                     image[i][j][k] = 0
     return image
 
-
+#Apply Threshold function
 image = Threshold(image)
 
+#Export image
 mpimg.imsave('Threshold.png', image)
 
