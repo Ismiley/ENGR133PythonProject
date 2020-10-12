4c2b7e12ef1dbcf6b4c18dd3f6c30baf8b19f4a7
mkane8301 <72466229+mkane8301@users.noreply.github.com>
2020-10-12 16:17:31 -0400

    Add files via upload

---
+++ Sobel_Edge_Detection.py
@@ -0,0 +1,46 @@
+
+'''
+===============================================================================
+ENGR 133 Fa 2020
+
+Assignment Information
+	Assignment:     Python Team Project: Image Analysis
+	Author:         Matthew Kane, kane83@purdue.edu
+	Team ID:        LC5-005
+	
+Contributors:   Matthew Kane, kane83@purdue.edu
+                Dominic Caponigo, dcoponig@purdue.edu
+                Avneesh Viswanath, viswan16@purdue.edu
+                Ismail Husain, husain6@purdue.edu
+
+	My contributor(s) helped me:	
+	[ ] understand the assignment expectations without
+		telling me how they will approach it.
+	[ ] understand different ways to think about a solution
+		without helping me plan my solution.
+	[ ] think through the meaning of a specific error or
+		bug present in my code without looking at my code.
+	Note that if you helped somebody else with their code, you
+	have to list that person as a contributor here as well.
+===============================================================================
+'''
+#The final edge detection program using the Sobel Algorithm 
+
+#Function to import the grayscale image
+
+#Function to perform blur/smoothing
+
+#Function to perform edge enhancement
+
+#Function to perform Sobel edge detection 
+
+'''
+===============================================================================
+ACADEMIC INTEGRITY STATEMENT
+    I have not used source code obtained from any other unauthorized
+    source, either modified or unmodified. Neither have I provided
+    access to my code to another. The project I am submitting
+    is my own original work.
+===============================================================================
+'''
+
