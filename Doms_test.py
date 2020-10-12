afcfe644c2916f2dead235177072a35ec5762a9b
DominickCaponigro <72466114+DominickCaponigro@users.noreply.github.com>
2020-10-12 18:29:27 -0400

    Add files via upload

--- Doms_test.py
+++ Doms_test.py
@@ -21,43 +21,47 @@ Contributors:   Ismail Husain, husain6@purdue
 	have to list that person as a contributor here as well.
 ===============================================================================
 '''
-import matplotlib.image as mpl
+def GrayScale():
+    import matplotlib.image as mpl
 
-class Error1(FileNotFoundError):
-    pass
+    class Error1(FileNotFoundError):
+        pass
 
-try:
-    Input=input('File Name: ')
-except FileNotFoundError:
-    raise Error1('File name incorrect, dont forget the file type')
+    try:
+        Input=input('File Name: ')
+    except FileNotFoundError:
+        raise Error1('File name incorrect, dont forget the file type')
 
-# Takes a file path and returns the array of the image
-def importImage(x): #x is the file path as a string
-    global image # to show image in variable explorer for analysis and later use
-    image = mpl.imread(x)
+    # Takes a file path and returns the array of the image
+    def importImage(x): #x is the file path as a string
+        global image # to show image in variable explorer for analysis and later use
+        image = mpl.imread(x)
+        return image
+
+    #Takes he image and and makes it gray by changing the color values of each pixel.
+    def RGBtoGRAY(rgb):
+        global gray # to show image in cariable explorer for analysis
+        for i in range(len(rgb)):
+            for j in range(len(rgb[0])):
+                # Gets the RGB values for a pixel
+                r=rgb[i][j][0]
+                g=rgb[i][j][1]
+                b=rgb[i][j][2]
+                # Calculates new color value grayscaled
+                gray=(0.2989*r+0.5870*g+0.1140*b)
+                # Assignes new gray value to old RGB values
+                rgb[i][j][0],rgb[i][j][1],rgb[i][j][2]=gray,gray,gray
+        return rgb
+
+    # Opens an output file, runs applicable functions, then closes
+    GOutput=open('Grayscale.png','wb')
+    importImage(Input)
+    RGBtoGRAY(image)
+    mpl.imsave('Grayscale.png',RGBtoGRAY(image))
+    GOutput.close
     return image
 
-#Takes he image and and makes it gray by changing the color values of each pixel.
-def RGBtoGRAY(rgb):
-    global gray # to show image in cariable explorer for analysis
-    for i in range(len(rgb)):
-        for j in range(len(rgb[0])):
-            # Gets the RGB values for a pixel
-            r=rgb[i][j][0]
-            g=rgb[i][j][1]
-            b=rgb[i][j][2]
-            # Calculates new color value grayscaled
-            gray=(0.2989*r+0.5870*g+0.1140*b)
-            # Assignes new gray value to old RGB values
-            rgb[i][j][0],rgb[i][j][1],rgb[i][j][2]=gray,gray,gray
-    return rgb
-
-# Opens an output file, runs applicable functions, then closes
-GOutput=open('Grayscale.png','wb')
-importImage(Input)
-RGBtoGRAY(image)
-mpl.imsave('Grayscale.png',RGBtoGRAY(image))
-GOutput.close
+GrayScale()
 
 '''
 ===============================================================================
