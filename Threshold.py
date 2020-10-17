5e6381b5c6e1caac552cf509dcc75a4fb5104c1d
Ismail Husain <husaisma@gmail.com>
2020-10-17 11:15:53 -0500

    Added automatic threshold calculator

--- Threshold.py
+++ Threshold.py
@@ -9,6 +9,7 @@ Created on Tue Oct  6 15:03:52 2020
 
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+import numpy as np
 
 #get image location
 #path = input('Input image file name (in folder): ')
@@ -24,19 +25,47 @@ def importImage(path):
 #converts to black (0) or white (255) if threshold value is met
 def Threshold(image):
     #image = mpimg.imread("Sobel_edge_enhanced.png")
+    #image = mpimg.imread("Grayscale.png")
     thresholdVal = -1
-    while thresholdVal > 255 or thresholdVal < 0:
-        thresholdVal = int(input("Enter the desired threshold value [Recommended=25]: "))
-        if thresholdVal > 255 or thresholdVal < 0:
-            print("Please enter a valid threshold value!")
+
+    #Total number of bins in histogram
+    bins_num = 256
+
+    #Get histogram
+    hist, bin_edges = np.histogram(image, bins=bins_num)
+
+    #Calc center of bins
+    bin_mids = (bin_edges[:-1] + bin_edges[1:]) / 2.
+
+    #Get probabilities for thresholds
+    weight1 = np.cumsum(hist)
+    weight2 = np.cumsum(hist[::-1])[::-1]
+
+    #Get means of probabilities
+    mean1 = np.cumsum(hist * bin_mids) / weight1
+    mean2 = (np.cumsum((hist * bin_mids)[::-1]) / weight2[::-1])[::-1]
+    inter_class_variance = weight1[:-1] * weight2[1:] * (mean1[:-1] - mean2[1:]) ** 2
+
+    #Maximize inter_class_variance
+    index_of_max_val = np.argmax(inter_class_variance)
+    thresholdVal2 = bin_mids[:-1][index_of_max_val]
+
+    #Convert threshold to 8-bit value
+    thresholdVal2 = thresholdVal2 * 255
+
+    #while thresholdVal > 255 or thresholdVal < 0:
+    #    thresholdVal = int(input("Enter the desired threshold value [Recommended=25]: "))
+    #    if thresholdVal > 255 or thresholdVal < 0:
+    #        print("Please enter a valid threshold value!")
+
     iRows = len(image)
     iCols = len(image[0])
     iVals = len(image[0][0])
     for i in range(iRows):
         for j in range(iCols):
-            for k in range(iVals):   
+            for k in range(iVals):
                 valRGB = 255 * image[i][j][k]
-                if valRGB > thresholdVal:
+                if valRGB > thresholdVal2:
                     image[i][j][k] = 1
                 else:
                     image[i][j][k] = 0
@@ -46,6 +75,4 @@ def Threshold(image):
     plt.show()
 
     return image
-
-
 
